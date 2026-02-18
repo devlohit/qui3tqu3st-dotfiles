b1cc1631ccb3827efc18ddb5f254b9f685fdd406
devlohit <iamlohitsofar@gmail.com>
2026-02-18 11:06:58 +0530

    changes in scripts

--- scripts/sc-laptop-toggle.sh
+++ scripts/sc-laptop-toggle.sh
@@ -6,9 +6,9 @@ LAPTOP="eDP-1"
 if hyprctl monitors | grep -q "$LAPTOP"; then
   # Laptop is enabled, disable it
   hyprctl keyword monitor "$LAPTOP,disable"
-  notify-send "Laptop Screen" "Disabled" -i display-brightness-off
+  # notify-send "Laptop Screen" "Disabled" -i display-brightness-off
 else
-  # Laptop is disabled, enable it
-  hyprctl keyword monitor "$LAPTOP,1920x1080@60,0x0,1"
-  notify-send "Laptop Screen" "Enabled" -i display-brightness
+  # Laptop is disabled, enable it and render it to left of external
+  hyprctl keyword monitor "$LAPTOP,highrr,auto,1"
+  # notify-send "Laptop Screen" "Enabled" -i display-brightness
 fi
