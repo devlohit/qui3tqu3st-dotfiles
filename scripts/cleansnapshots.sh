3744314abb19edf182382a7b186ac2f1038e79ca
devlohit <iamlohitsofar@gmail.com>
2026-02-21 15:32:08 +0530

    tweaked cleansnapper script

--- scripts/cleansnapshots.sh
+++ scripts/cleansnapshots.sh
@@ -3,7 +3,7 @@ echo "--- BTRFS SNAPPER SNAPSHOT CLEANING ---"
 TOTAL=$(sudo snapper list | awk 'NR>2 && $1!=0' | wc -l)
 echo "--- CURRENT SNAPSHOT COUNT : $TOTAL ---"
 
-KEEP=4
+KEEP=8
 if [ $TOTAL -gt $KEEP ]; then
   DELETE_COUNT=$((TOTAL - KEEP))
   FIRST=$(sudo snapper list | awk 'NR>2 && $1!=0 {print $1}' | head -n 1)
