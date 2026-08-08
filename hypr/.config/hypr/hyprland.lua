c65c2245e2d719c852607a72ee0289aef2e69aa1
devlohit <iamlohitsofar@gmail.com>
2026-08-08 20:00:32 +0530

    add new configs

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -2,30 +2,19 @@
 
 --- HYPRLAND CONFIGURATION ---
 
--- source = ~/.config/hypr/configs/keybindings.conf -> requires manual conversion
 local keybindings = require("configs.keybindings")
--- TODO: convert ~/.config/hypr/configs/keybindings.conf to .lua and use require()
 
--- source = ~/.config/hypr/configs/monitors.conf -> requires manual conversion
 local monitors = require("configs.monitors")
--- TODO: convert ~/.config/hypr/configs/monitors.conf to .lua and use require()
 
--- source = ~/.config/hypr/configs/autostart.conf -> requires manual conversion
 local autostart = require("configs.autostart")
--- TODO: convert ~/.config/hypr/configs/autostart.conf to .lua and use require()
 
--- source = ~/.config/hypr/configs/input.conf -> requires manual conversion
 local input = require("configs.input")
--- TODO: convert ~/.config/hypr/configs/input.conf to .lua and use require()
 
--- source = ~/.config/hypr/configs/ENV.conf -> requires manual conversion
 local ENV = require("configs.ENV")
--- TODO: convert ~/.config/hypr/configs/ENV.conf to .lua and use require()
 
--- source = ~/.config/hypr/configs/settings.conf -> requires manual conversion
 local settings = require("configs.settings")
--- TODO: convert ~/.config/hypr/configs/settings.conf to .lua and use require()
 
--- source = ~/.config/hypr/configs/visual.conf -> requires manual conversion
 local visual = require("configs.visual")
--- TODO: convert ~/.config/hypr/configs/visual.conf to .lua and use require()
+
+-- HYPRMOD --
+local hyprmod_hyprland_gui = require("hyprland-gui")
